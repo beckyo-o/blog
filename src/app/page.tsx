a1f6467d2ad5a3576b9122abf72e973b5d96888b
Becky Xie <uahtp1234@gmail.com>
2024-10-22 15:35:17 -0400

    kinda dif home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,51 @@
-'use client'
-import BlogTab from '@/comps/BlogTab'
-import entriesData from '@/entries/entry.json'
+"use client";
+import BlogTab from "@/comps/BlogTab";
+import entriesData from "@/entries/entry.json";
 
 const openBlog = (id: number) => {
-  window.location.href = `/entry/${id}`
+  window.location.href = `/entry/${id}`;
 };
 
 export default function Home() {
-
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-
-        <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <code className="font-mono font-bold">Goal:&nbsp;</code>
           Fix this home page.. really boring.
         </p>
       </div>
-      <div>
-        <h1 className='font-mono text-2xl font-semibold tracking-wide lg:tracking-widest sm:tracking-tight text-center items-center'>
-          Becky's really cool blog!
-        </h1>
-      </div>
-
-      <div className='mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left'>
-
-        {entriesData.map((entry) => (
-          <BlogTab
-            id={entry.id}
-            bod={entry.content}
-          />
-        ))}
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Check out Vercel {" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      <div className="px-6 py-24 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-2xl font-semibold tracking-tight sm:text-5xl">
+            Becky's really cool blog!
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-
-          </p>
-        </a>
-
+        </div>
+      </div>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {entriesData.map((entry) => (
+            <BlogTab id={entry.id} bod={entry.content} />
+          ))}
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              Check out Vercel{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              {" "}
+              i have to have this here, or else tailwind doesn't want to
+              compile..
+            </p>
+          </a>
+        </div>
       </div>
     </main>
   );
