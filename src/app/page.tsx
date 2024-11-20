d68ecf1fd051e044bd1418d18b708d4478bf4f0f
Becky Xie <uahtp1234@gmail.com>
2024-11-20 15:12:16 -0500

    typos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className='w-full max-w-5xl font-mono text-sm lg:flex'>
         <p className='left-0 top-0 flex justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
           <code className='font-mono font-bold'>Goal:&nbsp;</code>
-          Light mode.
+          Light mode. And DB.
         </p>
         <button className='rounded-full bg-purple-900 mx-5 px-2'>mystery ugly button...</button>
       </div>
